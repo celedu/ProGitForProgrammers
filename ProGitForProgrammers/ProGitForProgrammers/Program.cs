1e4bb28054a0721ecd5578b3ffd3e9e2d72068e9
Cel Edukacyjny <110299745+celedu@users.noreply.github.com>
2023-01-10 19:15:05 +0100

    Dodano nową linijkę w Visual Studio Code

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Właśnie dodałem linijkę w Visual Studio 2022");
+Console.WriteLine("Właśnie dodałem linijkę w Visual Studio Code");
