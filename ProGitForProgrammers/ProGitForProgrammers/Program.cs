808aebbb0619abcccad34c00659e9804a35f4c51
Cel Edukacyjny <110299745+celedu@users.noreply.github.com>
2023-01-17 18:33:19 +0100

    Ćwiczenia z indeksem lokalnym

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Właśnie dodałem linijkę w Visual Studio 2022");
 Console.WriteLine("Właśnie dodałem linijkę w Visual Studio Code");
+Console.WriteLine("Ta linia pokazuje 'staging area'");
