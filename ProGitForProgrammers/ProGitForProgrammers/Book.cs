e72c49ee72ce0154951dfa450d3a5f0eda9f885d
Cel Edukacyjny <110299745+celedu@users.noreply.github.com>
2023-01-23 18:33:44 +0100

    Dodano klasę Book

---
+++ ProGitForProgrammers/ProGitForProgrammers/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace ProGitForProgrammers;
+
+public class Book
+{
+    public string Title { get; set; }
+    public List<string> Authors { get; set; }
+    public DateTime PublicationDate { get; set; }
+}
